5559f940215f2791812743f1a79da7742cc58b12
sidiropulometin@gmail.com <sidiropulometin@gmail.com>
2023-01-11 17:57:22 +0600

    hwn3

---
+++ AspNewStudy/Controllers/InfoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNewStudy.Controllers
+{
+    public class InfoController : Controller
+    {
+        private void SetInfo(string browser, string Uagent, string url, string ip, string referrer)
+        {
+            ViewBag.browser = browser;
+            ViewBag.userAgent = Uagent;
+            ViewBag.url = url;
+            ViewBag.ip = ip;    
+            ViewBag.referrer = referrer;
+        }
+        public ActionResult UserInfo()
+        {
+            var req = HttpContext.Request;
+            SetInfo(req.Browser.Browser, req.UserAgent, req.RawUrl, req.UserHostAddress,
+                HttpContext.Request.UrlReferrer == null ? "" : HttpContext.Request.UrlReferrer.AbsoluteUri);
+            return View();
+        }
+    }
+}
