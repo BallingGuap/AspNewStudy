c6ccc0a7577f717769d69c896379989214db02b3
sidiropulometin@gmail.com <sidiropulometin@gmail.com>
2023-01-18 18:55:17 +0600

    Home work №6

---
+++ AspNewStudy/Controllers/DeleteController.cs
@@ -0,0 +1,41 @@
+﻿using AspNewStudy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Account = AspNewStudy.Models.Account;
+
+namespace AspNewStudy.Controllers
+{
+    public class DeleteController : Controller
+    {
+        AccountContext context = AccountDbSingelton.dbSingl;
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            Account account = context.Accounts.Find(id);
+
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Account account = context.Accounts.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+
+            context.Accounts.Remove(account);
+            context.SaveChanges();
+            return RedirectToAction("../Account/Index");
+        }
+
+    }
+}
---
+++ AspNewStudy/Controllers/EditController.cs
@@ -0,0 +1,48 @@
+﻿using AspNewStudy.Migrations;
+using AspNewStudy.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using Account = AspNewStudy.Models.Account;
+
+namespace AspNewStudy.Controllers
+{
+    public class EditController : Controller
+    {
+        AccountContext context = AccountDbSingelton.dbSingl;
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+          
+                if (id == null)
+                {
+                    return HttpNotFound();
+                }
+
+                Account account = context.Accounts.Find(id);
+
+                if (account != null)
+                {
+                    return View(account);
+                }
+
+                return HttpNotFound();
+        }
+
+        [HttpPost, ActionName("Edit")]
+        public ActionResult Edit(Account account)
+        {
+
+            context.Set<Account>().AddOrUpdate(account);
+            //context.Entry(account).State = EntityState.Modified;
+            context.SaveChanges(); 
+            return RedirectToAction("../Account/Index");
+        }
+    }
+}
