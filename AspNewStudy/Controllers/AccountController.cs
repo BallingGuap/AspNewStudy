a455789149fd0104d5e4d2f1dc12a7556363033c
sidiropulometin@gmail.com <sidiropulometin@gmail.com>
2023-01-21 23:24:55 +0600

    Home Work №8

--- AspNewStudy/Controllers/AccountController.cs
+++ AspNewStudy/Controllers/AccountController.cs
@@ -1,21 +1,57 @@
 ﻿using AspNewStudy.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace AspNewStudy.Controllers
 {
+  
+
     public class AccountController : Controller
     {
         // GET: Account
+
+
+       
+
+
         AccountContext context = AccountDbSingelton.dbSingl;
+        IEnumerable<Account> accounts = null;
+
+        public AccountController() :
+            base() =>
+             accounts = context.Accounts;
+
+      
         public ActionResult Index()
         {
-            IEnumerable<Account> accounts = context.Accounts;
-            ViewBag.Accounts = accounts;
+            ViewBag.Accounts = accounts;    
             return View();
         }
+
+
+        public ActionResult AccountPagination(int page = 1)
+        {
+            int pageSize = 2;
+
+            IQueryable<Account> source = context.Accounts;
+         
+            var count =  source.Count();
+            var items = source.OrderBy(acc => acc.ID).Skip((page - 1) * pageSize).Take(pageSize).ToList();
+
+            PageViewModel pageViewModel = new PageViewModel(count, page, pageSize);
+            IndexViewModel viewModel = new IndexViewModel
+            {
+                PageViewModel = pageViewModel,
+                Users = items
+            };
+            return View(viewModel);
+        }
+
     }
 }
---
+++ AspNewStudy/Models/IndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNewStudy.Models
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<Account> Users { get; set; }
+        public PageViewModel PageViewModel { get; set; }
+    }
+}
