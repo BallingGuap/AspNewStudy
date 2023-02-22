9cfe9418ea9a7fad215dc0b956acc8d2665ebe66
sidiropulometin@gmail.com <sidiropulometin@gmail.com>
2023-02-22 22:43:18 +0600

    Home Work №17

---
+++ AspNewStudy/Controllers/QuestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AspNewStudy.Models;
+
+namespace AspNewStudy.Controllers
+{
+    public class QuestController : Controller
+    {
+        // GET: Quest
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult StylizedQuest()
+        {
+            return View(new Quest());
+        }
+    }
+}
---
+++ AspNewStudy/Models/Quest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNewStudy.Models
+{
+    public class Quest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        public DateTime Time { get; set; }
+        public int MinPlayersCount { get; set; }
+        public int MaxPlayersCount { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Company { get; set; }
+        public int Rating { get; set; }
+        public string FearScale { get; set; }
+        public string HardScale { get; set; }
+        public string Logo { get; set; }
+        public IEnumerable<string> Photos { get; set; }
+    }
+}
